474f583a8986f76695756cbc8176e1ca20dd4eb1
Kurt Van Dijck <kurt@vandijck-laurijssen.be>
2013-03-01 16:55:43 +0100

    linumatic: add poweroff
    Signed-off-by: Kurt Van Dijck <kurt@vandijck-laurijssen.be>

--- arch/avr32/boards/icnova/linumatic.c
+++ arch/avr32/boards/icnova/linumatic.c
@@ -11,6 +11,28 @@
 #include <mach/init.h>
 #include <mach/board.h>
 
+#include <linux/pm.h>
+#include <linux/gpio.h>
+#include <mach/at32ap700x.h>
+
+/* define the poweroff gpio on linumatic */
+#define POWEROFF_PIN GPIO_PIN_PA(26)
+
+static void linumatic_pm_power_off(void)
+{
+	gpio_set_value(POWEROFF_PIN, 1);
+}
+
+static int __init linumatic_set_poweroff(void)
+{
+	if (gpio_request(POWEROFF_PIN, "linumatic poweroff") == 0) {
+		gpio_direction_output(POWEROFF_PIN, 0);
+		pm_power_off = linumatic_pm_power_off;
+	}
+	return 0;
+}
+late_initcall(linumatic_set_poweroff);
+
 void __init setup_board(void)
 {
 	at32_map_usart(1, 0, 0);	// USART 1: /dev/ttyS0
